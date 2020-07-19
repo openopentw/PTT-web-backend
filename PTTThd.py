f4415054a54141b3cf80728650980be286a7690b
YJC <openopentw@gmail.com>
2020-07-19 14:49:25 +0800

    Add current working program.

---
+++ PTTThd.py
@@ -0,0 +1,127 @@
+import threading
+
+from PyPtt import PTT
+
+class PTTThd(threading.Thread):
+    def __init__(self, data):
+        super(PTTThd, self).__init__(daemon=True)
+        self.data = data
+        self.bot = None
+        self.timeout_sec = 80
+
+    # intermediary api
+
+    def get_posts(self, board_name, end_idx, num=20):
+        posts = []
+        beg_idx = end_idx - num + 1
+        self.bot.crawl_board(PTT.data_type.crawl_type.BBS,
+                             lambda p, posts=posts: posts.append(p),
+                             board_name,
+                             start_index=beg_idx,
+                             end_index=end_idx)
+
+        ret = []
+        for p in posts:
+            p.__dict__['push_list'] = [push.__dict__ for push in p.__dict__['push_list']]
+            ret.append(p.__dict__)
+        return ret
+
+    # cmds
+
+    def cmd_login(self):
+        bot = self.bot
+        self.data.clear_status()
+        status = self.data.status
+
+        try:
+            bot.login(self.data.param['user'], self.data.param['pass'])
+            bot.log('登入成功')
+            status['status'] = True
+
+            if bot.unregistered_user:
+                bot.log('未註冊使用者')
+                status['str'] = '未註冊使用者'
+                if bot.process_picks != 0:
+                    bot.log(f'註冊單處理順位 {bot.process_picks}')
+                    status['str'] += f'\n註冊單處理順位 {bot.process_picks}'
+
+            else:
+                fav_b = self.bot.get_favourite_board()
+                fav_b = [b.__dict__ for b in fav_b]
+                self.data.data['fav_b'] = fav_b
+
+        except PTT.exceptions.LoginError:
+            bot.log('登入失敗')
+            self.data.set_status(False, '登入失敗')
+        except PTT.exceptions.WrongIDorPassword:
+            bot.log('帳號密碼錯誤')
+            self.data.set_status(False, '帳號密碼錯誤')
+        except PTT.exceptions.LoginTooOften:
+            bot.log('請稍等一下再登入')
+            self.data.set_status(False, '請稍等一下再登入')
+
+    def cmd_logout(self):
+        self.bot.logout()
+        self.data.set_status(True, '')
+
+    def cmd_get_fav_board(self):
+        fav_b = self.bot.get_favourite_board()
+        fav_b = [b.__dict__ for b in fav_b]
+        self.data.set_status(True, '')
+        self.data.data['fav_b'] = fav_b
+
+    def cmd_get_posts(self):
+        b_name = self.data.param['board_name']
+        end_idx = self.data.param['end_idx']
+        if end_idx == 'recent':
+            end_idx = self.bot.get_newest_index(PTT.data_type.index_type.BBS, board=b_name)
+        posts = self.get_posts(b_name, end_idx)
+
+        self.data.clear_status()
+        self.data.status['status'] = True
+        self.data.data['posts'] = posts
+
+    # run
+
+    def run(self):
+        self.bot = PTT.API()
+        data = self.data
+        cond = data.cond
+        cond.acquire()
+        while True:
+            # try to login
+            while data.cmd != 'login':
+                data.set_status(False, 'Please login first')
+                cond.notify()
+                cond.wait()
+            self.cmd_login()
+            cond.notify()
+            # operations after login
+            if data.status['status']: # login successfully
+                data.timeout = True
+                cond.wait(self.timeout_sec)
+                while data.cmd != 'logout' and not data.timeout:
+                    if data.cmd == 'get_fav_board':
+                        self.cmd_get_fav_board()
+                    elif data.cmd == 'get_posts':
+                        self.cmd_get_posts()
+                    # TODO: add other PTT commands
+                    else:
+                        data.set_status(False, 'No this command')
+                    data.timeout = True
+                    cond.notify()
+                    cond.wait(self.timeout_sec)
+                self.cmd_logout()
+                if data.timeout:
+                    data.glb_list['used'][data.id].lock.acquire()
+                    cur_data = data.glb_list['used'][data.id]
+                    data.glb_list['used'].pop(data.id, None)
+                    data.glb_list['available'].append(cur_data)
+                    cur_data.clear()
+                    cur_data.lock.release()
+                else:
+                    cond.notify()
+            # wait for another login
+            cond.wait()
+        cond.notify()
+        cond.release()
---
+++ main.py
@@ -0,0 +1,170 @@
+import argparse
+import atexit
+import os
+import time
+import threading
+
+from flask import Flask, jsonify, session, request
+# from flask_session.__init__ import Session
+
+from PTTData import PTTData
+
+DATA = {
+    'used': {},
+    'available': [],
+}
+
+app = Flask(__name__)
+# app.secret_key = b'\x1e<d\x14\xbc\x1a\xb1,\x9ck/}\xb7"\xa0\x00'
+app.secret_key = os.urandom(32)
+# app.config['SESSION_TYPE'] = 'filesystem'
+# Session(app)
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--port', default=8008)
+    parser.add_argument('-d', '--debug', action='store_true', default=False)
+    args = parser.parse_args()
+    return args
+
+@app.route('/api/user')
+def user():
+    if 'user' in session:
+        print('{} has logged in before'.format(session['user']))
+        return {'status': True, 'user': session['user']}
+    return {'status': False, 'str': 'haven\'t logged in'}
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    res = request.get_json()
+    print('{} login'.format(res['user']))
+    id_ = res['user']
+
+    if 'user' in session:
+        # TODO: redirect
+        return {'status': False, 'str': 'Already logged in other tabs.'}
+    if id_ in DATA['used']:
+        # TODO: enable multiple user
+        return {'status': False, 'str': 'Already logged in other device.'}
+
+    is_new_thread = False
+    data = None
+    try:
+        data = DATA['available'].pop()
+    except IndexError:
+        data = PTTData(id_, DATA)
+        is_new_thread = True
+    data.lock.acquire()
+    data.id = id_
+
+    data.cond.acquire()
+    data.set_cmd('login', {'user': res['user'], 'pass': res['pass']})
+    if is_new_thread:
+        data.thd.start()
+    else:
+        data.cond.notify()
+    data.cond.wait()
+    status = dict(data.status)
+    data.clear()
+    data.cond.release()
+
+    if status['status']:
+        DATA['used'][id_] = data
+        session['user'] = id_
+    else:
+        DATA['available'].append(data)
+    data.lock.release()
+
+    return status
+
+@app.route('/api/logout')
+def logout():
+    id_ = session['user']
+    print('{} logout'.format(id_))
+
+    DATA['used'][id_].lock.acquire()
+    data = DATA['used'][id_]
+
+    data.cond.acquire()
+    data.set_cmd('logout')
+    data.cond.notify()
+    data.cond.wait()
+    status = dict(data.status)
+    data.clear()
+    data.cond.release()
+
+    DATA['used'].pop(id_, None)
+    DATA['available'].append(data)
+    data.lock.release()
+
+    session.pop('user', None)
+    return status
+
+@app.route('/api/get_fav_board')
+def get_fav_board():
+    id_ = session['user']
+    print('{} get_fav_board'.format(id_))
+
+    DATA['used'][id_].lock.acquire()
+    data = DATA['used'][id_]
+
+    data.cond.acquire()
+    data.set_cmd('get_fav_board')
+    data.cond.notify()
+    data.cond.wait()
+    status = dict(data.status)
+    ret_data = dict(data.data)
+    data.cond.release()
+
+    data.lock.release()
+
+    return {'status': status, 'data': ret_data}
+
+@app.route('/api/get_posts', methods=['POST'])
+def get_posts():
+    id_ = session['user']
+    print('{} get_posts'.format(id_))
+    res = request.get_json()
+
+    DATA['used'][id_].lock.acquire()
+    data = DATA['used'][id_]
+
+    data.cond.acquire()
+    data.set_cmd('get_posts',
+                 {'board_name': res['board_name'],
+                  'end_idx': res['end_idx']})
+    data.cond.notify()
+    data.cond.wait()
+    status = dict(data.status)
+    ret_data = dict(data.data)
+    data.cond.release()
+
+    data.lock.release()
+
+    return {'status': status, 'data': ret_data}
+
+@app.route('/api')
+def api():
+    s = 'Hello, API!'
+    return jsonify(s)
+
+@atexit.register
+def logout_all():
+    print('Logging out all users ...')
+    for id_ in DATA['used']:
+        DATA['used'][id_].cond.acquire()
+        data = DATA['used'][id_]
+        data.cmd = 'logout'
+        data.timeout = False
+        data.cond.notify()
+        data.cond.wait()
+        status = dict(data.status)
+        data.clear()
+        data.cond.release()
+        if data.thd.is_alive():
+            data.thd.join()
+        del DATA['used'][id_]
+
+if __name__ == '__main__':
+    args = parse_args()
+    app.run(host='0.0.0.0', port=args.port, debug=args.debug)
